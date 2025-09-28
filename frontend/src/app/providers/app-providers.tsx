2a7abc35534679c28154f40a36cc0ae129b297c3
Orbitado <juanpaavaldezz@gmail.com>
2025-09-27 22:03:40 -0300

    feat: add Toaster component using Sonner and integrate it into AppProviders

--- frontend/src/app/providers/app-providers.tsx
+++ frontend/src/app/providers/app-providers.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { QueryProvider } from "./query-provider";
+import { Toaster } from "sonner";
 
 interface AppProvidersProps {
   children: ReactNode;
@@ -10,6 +11,7 @@ export function AppProviders({ children }: AppProvidersProps) {
   return (
     <HelmetProvider>
       <QueryProvider>{children}</QueryProvider>
+      <Toaster position="bottom-right" richColors closeButton />
     </HelmetProvider>
   );
 }
